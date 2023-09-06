43d54d643d1fcd1b139f73480b589229aca4a9cc
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-06 20:05:25 +0300

    Feat: seminar 1 class Main

--- src/seminar1/Main.java
+++ src/seminar1/Main.java
@@ -2,6 +2,7 @@ package seminar1;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[][] matrix = Matrix.getMatrix(3);
+        Formatter.printMatrix(matrix);
     }
 }
