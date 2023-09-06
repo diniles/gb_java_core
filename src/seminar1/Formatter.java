6b36ed658d2b54786b34eafa12766552aff2c41d
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-06 20:12:38 +0300

    Feat: seminar 1 class Formatter

--- src/seminar1/Formatter.java
+++ src/seminar1/Formatter.java
@@ -2,15 +2,12 @@ package seminar1;
 
 public class Formatter {
     public static void printMatrix(int[][] matrix) {
-        int length = matrix.length;
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("| ");
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                stringBuffer.append(matrix[i][j]).append(" |");
+        for (int[] row : matrix) {
+            for (int elem : row) {
+                System.out.printf("%4d", elem);
             }
-            stringBuffer.append("]n");
+            System.out.println();
         }
-        System.out.println(stringBuffer);
+        System.out.println();
     }
 }
