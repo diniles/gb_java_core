ad8c8ccfcfd4370883309a6468d1994985fd03b1
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-06 20:05:05 +0300

    Feat: seminar 1 class Matrix

--- src/seminar1/Matrix.java
+++ src/seminar1/Matrix.java
@@ -2,7 +2,7 @@ package seminar1;
 
 public class Matrix {
 
-    public int[][] getMatrix(int size) {
+    public static int[][] getMatrix(int size) {
         int[][] matrix = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
