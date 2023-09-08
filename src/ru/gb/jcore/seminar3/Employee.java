76c90e01094cac7c8b4ed083e5b016a13b1ca709
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-08 18:46:21 +0300

    Feat: seminar 3 class Employee

---
+++ src/ru/gb/jcore/seminar3/Employee.java
@@ -0,0 +1,27 @@
+package ru.gb.jcore.seminar3;
+
+public abstract class Employee {
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public abstract double salary();
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+}
