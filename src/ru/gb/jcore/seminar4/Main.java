fc20a31aedb4d56795b0d80341b8e841b253d13d
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-10 14:26:52 +0300

    Feat: seminar4 class Main

--- src/ru/gb/jcore/seminar4/Main.java
+++ src/ru/gb/jcore/seminar4/Main.java
@@ -4,31 +4,26 @@ import ru.gb.jcore.seminar3.MyArrayDataException;
 
 public class Main {
     private static final int MATRIX_SIZE = 4;
-    private static final String[][] MATRIX = {
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"}
-    };
 
 
     public static void main(String[] args) {
-        String[][] matrix = MATRIX;
-        checkMatrixSize(matrix);
-        System.out.println(sumOfMatrix(matrix));
+        final String[][] MATRIX = {
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}
+        };
+        System.out.println(sumOfMatrix(MATRIX));
     }
 
-    private static void checkMatrixSize(String[][] matrix) {
+    private static int sumOfMatrix(String[][] matrix) {
         if (matrix.length != 4)
             throw new MyArraySizeException(MATRIX_SIZE, matrix.length, "lines");
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i].length != 4) {
-                throw new MyArraySizeException(MATRIX_SIZE, matrix.length, "cols in line " + (i + 1));
+                throw new MyArraySizeException(MATRIX_SIZE, matrix[i].length, "cols in line " + (i + 1));
             }
         }
-    }
-
-    private static int sumOfMatrix(String[][] matrix) {
         int sum = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
