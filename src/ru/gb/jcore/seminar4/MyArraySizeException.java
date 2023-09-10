deba6c3758533b6b4233be08478a5e16f51b2432
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-10 14:22:28 +0300

    Feat: seminar4 class MyArraySizeException

--- src/ru/gb/jcore/seminar4/MyArraySizeException.java
+++ src/ru/gb/jcore/seminar4/MyArraySizeException.java
@@ -2,6 +2,6 @@ package ru.gb.jcore.seminar4;
 
 public class MyArraySizeException extends RuntimeException {
     MyArraySizeException(int expectedSize, int size, String message) {
-        super(String.format("Expected %d %s", expectedSize, message));
+        super(String.format("Expected %d but received %d size %s", expectedSize, size, message));
     }
 }
