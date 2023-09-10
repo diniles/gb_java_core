bb1b596ded2e5722e1e9b765c173b3c2f7ce1c82
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-10 14:19:49 +0300

    Feat: seminar4 class Main

--- src/ru/gb/jcore/seminar4/Main.java
+++ src/ru/gb/jcore/seminar4/Main.java
@@ -1,5 +1,7 @@
 package ru.gb.jcore.seminar4;
 
+import ru.gb.jcore.seminar3.MyArrayDataException;
+
 public class Main {
     private static final int MATRIX_SIZE = 4;
     private static final String[][] MATRIX = {
@@ -9,6 +11,12 @@ public class Main {
             {"1", "2", "3", "4"}
     };
 
+
+    public static void main(String[] args) {
+        String[][] matrix = MATRIX;
+        checkMatrixSize(matrix);
+        System.out.println(sumOfMatrix(matrix));
+    }
 
     private static void checkMatrixSize(String[][] matrix) {
         if (matrix.length != 4)
@@ -20,9 +28,17 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        String[][] matrix = MATRIX;
-        checkMatrixSize(matrix);
+    private static int sumOfMatrix(String[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(matrix[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
     }
-
 }
