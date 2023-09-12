d88f46076c350b983f6687da074a9547ed78a13c
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-12 18:27:43 +0300

    Feat: seminar 5 class Main

--- src/ru/gb/jcore/seminar5/Main.java
+++ src/ru/gb/jcore/seminar5/Main.java
@@ -1,11 +1,10 @@
 package ru.gb.jcore.seminar5;
 
-import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        CopyFiles.
+        CopyFiles.copy(".");
     }
 
 }
