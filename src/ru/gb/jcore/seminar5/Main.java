19eb1bb6cc4cd5078ef6b4cdfe80b0eb5d5f5f78
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-12 18:32:49 +0300

    Feat: seminar 5 class Main

--- src/ru/gb/jcore/seminar5/Main.java
+++ src/ru/gb/jcore/seminar5/Main.java
@@ -1,9 +1,11 @@
 package ru.gb.jcore.seminar5;
 
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CopyFiles.copy(".");
     }
 
