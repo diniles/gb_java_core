0ba51638e3556e284316661e230999cb06c5e8b6
Dzmitry Liashko <dzliashko@gmail.com>
2023-09-20 10:42:09 +0300

    Feat: seminar 5 class Copy

--- src/ru/gb/jcore/seminar5/Copy.java
+++ src/ru/gb/jcore/seminar5/Copy.java
@@ -34,9 +34,9 @@ public class Copy {
                     }
                     copyFolder(src + File.separator + file, dst + File.separator + file);
                 }
-            } else {
-                copyFile(fldSrc, fldDst);
             }
+        } else {
+            copyFile(fldSrc, fldDst);
         }
     }
 }
